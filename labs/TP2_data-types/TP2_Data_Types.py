e1a93417689dc47234266e4d0b912599f51e7952
Bingzhi <bingzhi.li@yahoo.com>
2021-09-23 17:31:05 +0200

    add tp2

---
+++ labs/TP2_data-types/TP2_Data_Types.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# TP2 Data Types and `if` Structure
+
+"""
+Original file is located at
+    https://colab.research.google.com/drive/1UbrFwL_jOAu_HVXvM_SxctZUBptFmM2f
+"""
+
+
+# Exercise 1 - Even or odd numbers? 
+#Ask the user to enter a number then display whether it is even or odd?
+
+#1. TODO
+
+
+
+"""Exercise 2 - Ticket price
+The price for visting a castle is as following:
++  free for children between 0 and 11 years old
++  5 euros for children between 12 and 17 years old and for adults over 60
++  10 euros for adults (18-59)
+
+Write  a  program  that  asks  for  the  user’s  age  and  returns  the  price  charged.
+"""
+
+#2. TODO
+
+
+
+
+
+
+
+
+"""text{Exercise 3 - How much to pay employees? 
+Write a program to ask the user for hours and rate per hour to compute gross pay, give the employee **1.5 times** the hourly rate for hours worked above **40 hours**.
+"""
+
+#3. TODO
+
+
+
+
+
+
+
+
+"""Exercise 4 - Which type?
+Write a program to check the type of input that the user entered from the keyboard. 
+Hints:
+- `input()` function gets the user's input(which can be `str`,`int`,`float`... data) and returns always a string object. 
+-  You can use `str()`,`int()` and `float()`functions in this exercice 
+"""
+
+user_input = input("Enter something:")
+#TODO
+
+
+
+
+
+"""Exercise 5 - Handle Non-numeric Input
+Rewrite your pay program of exercise 3 so that your program handles non-numeric input.
+
+"""
+
+#TODO
+
+
+
+
+
+
+
+
